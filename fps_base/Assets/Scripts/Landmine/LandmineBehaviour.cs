3da63a49a93cf1b5be3d161092df43145aeea40e
Kim <49259658+kimgeurae@users.noreply.github.com>
2019-05-02 00:12:00 -0300

    LandmineUpdate
    Added Landmine

---
+++ fps_base/Assets/Scripts/Landmine/LandmineBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandmineBehaviour : MonoBehaviour
+{
+
+    private Light blinkingLight;
+
+    void Start()
+    {
+        LoadReferencesAndSetValues();
+        StartCoroutine("BlinkLight");
+
+    }
+
+    void LoadReferencesAndSetValues()
+    {
+        blinkingLight = transform.GetChild(2).gameObject.GetComponent<Light>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator BlinkLight()
+    {
+        yield return new WaitForSeconds(1);
+        while (true)
+        {
+            blinkingLight.enabled = !blinkingLight.enabled;
+        }
+    }
+
+}
